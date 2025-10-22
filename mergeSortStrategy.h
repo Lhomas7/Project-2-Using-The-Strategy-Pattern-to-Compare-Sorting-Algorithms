66da5feda94af8573615de8ae4de0b4d92eb5105
Landon Davis Thomas <landon7@babbage2.computing.clemson.edu>
2025-10-22 10:20:15 -0400

    added definitions for mergeSort

---
+++ mergeSortStrategy.h
@@ -0,0 +1,15 @@
+#ifndef MERGESORTSTRATEGY_H
+#define MERGESORTSTRATEGY_H
+
+#include <iostream>
+#include <vector>
+#include "sortStrategy.h"
+
+template <typename T>
+class MergeSortStrategy : public SortStrategy<T> {
+	public:
+		MergeSortStrategy() {}
+		virtual void sort(std::vector<T>&) override;
+};
+
+#endif
