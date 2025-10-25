3886de7c2c4ffe4d837051cae47d4e1b5442592c
Landon Davis Thomas <landon7@babbage2.computing.clemson.edu>
2025-10-25 15:29:31 -0400

    added context functions

--- sortStrategyContext.h
+++ sortStrategyContext.h
@@ -9,6 +9,15 @@ class SortStrategyContext {
 	public:
 		SortStrategyContext(SortStrategy<T>* s) : sortStrategy(s) {}
 		void setStrategy(SortStrategy<T>* s) {sortStrategy = s;}
+		void sort(std::vector<T>& data) {
+			sortStrategy->sort(data);
+		}
+		void makeHeap(std::vector<T>& data) {
+			sortStrategy->makeHeap(data);
+		}
+		void print(std::string& msg, std::vector<T>& data) {
+			sortStrategy->print(msg, data);
+		}
 
 	private:
 		SortStrategy<T>* sortStrategy;
