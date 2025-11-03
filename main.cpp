9d9f90f203524cc0d2fced755eb3303c078b5bb2
Benjamin Newton <newton-03@hotmail.com>
2025-11-02 19:55:46 -0500

    Added Video

--- main.cpp
+++ main.cpp
@@ -46,34 +46,34 @@ int main() {
 	context.sort(s1,datacopy);
 
 	//Integer testing
-	std::vector<int> newData;
-	newData.reserve(MAX);
-	for (unsigned i = 0; i < MAX; ++i) {
-		newData.push_back(rand()%100);
-	}
+	// std::vector<int> newData;
+	// newData.reserve(MAX);
+	// for (unsigned i = 0; i < MAX; ++i) {
+	// 	newData.push_back(rand()%100);
+	// }
 
-	std::vector<int> newDataCopy = newData;
-	std::cout << "Size of data being sorted: " << MAX << "\n" << std::endl;
+	// std::vector<int> newDataCopy = newData;
+	// std::cout << "Size of data being sorted: " << MAX << "\n" << std::endl;
 
-	MergeSortStrategy<int> newMerge;
-	HeapSortStrategy<int> newHeap;
-	BubbleSortStrategy<int> newBubble;
+	// MergeSortStrategy<int> newMerge;
+	// HeapSortStrategy<int> newHeap;
+	// BubbleSortStrategy<int> newBubble;
 
-	SortStrategyContext<int> newContext(&newBubble);
-	s1 = "Unsorted Data; First 10:";
-	newContext.print(s1, newData);
-	s1 = "\nBubbleSort First 10: ";
-	newContext.sort(s1,newDataCopy);
+	// SortStrategyContext<int> newContext(&newBubble);
+	// s1 = "Unsorted Data; First 10:";
+	// newContext.print(s1, newData);
+	// s1 = "\nBubbleSort First 10: ";
+	// newContext.sort(s1,newDataCopy);
 
-	newDataCopy = newData;
-	newContext.setStrategy(&newMerge);
-	s1 = "MergeSort First 10: ";
-	newContext.sort(s1,newDataCopy);
+	// newDataCopy = newData;
+	// newContext.setStrategy(&newMerge);
+	// s1 = "MergeSort First 10: ";
+	// newContext.sort(s1,newDataCopy);
 
-	newDataCopy = newData;
-	newContext.setStrategy(&newHeap);
-	s1 = "HeapSort First 10: ";
-	newContext.sort(s1,newDataCopy);
+	// newDataCopy = newData;
+	// newContext.setStrategy(&newHeap);
+	// s1 = "HeapSort First 10: ";
+	// newContext.sort(s1,newDataCopy);
 	
-	return 0;
+	// return 0;
 }
